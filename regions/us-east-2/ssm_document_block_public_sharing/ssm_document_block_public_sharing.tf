719bc5c00c949a71be05102f6cb0951586fc6ad9
Michael McClelland <mccmcc@amazon.com>
2024-04-27 21:39:33 -0400

    code

---
+++ regions/us-east-2/ssm_document_block_public_sharing/ssm_document_block_public_sharing.tf
@@ -0,0 +1,12 @@
+resource "aws_ssm_service_setting" "test_setting" {
+  setting_id    = "arn:${data.aws_partition.partition.id}:ssm:${data.aws_region.region.id}:${data.aws_caller_identity.caller.id}:servicesetting/ssm/documents/console/public-sharing-permission"
+  setting_value = "Enable"
+}
+data "aws_caller_identity" "caller" {}
+data "aws_partition" "partition" {}
+data "aws_region" "region" {}
+
+terraform {
+  backend "s3" {
+  }
+}
