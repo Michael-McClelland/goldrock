d60d9822074bce5da2a3327027f023cf99a1039a
Michael McClelland <mccmcc@amazon.com>
2024-05-09 15:06:21 -0400

    code

--- state/main.tf
+++ state/main.tf
@@ -1,35 +1,3 @@
-resource "aws_organizations_organization" "organization" {
-  aws_service_access_principals = [
-    "access-analyzer.amazonaws.com",
-    "account.amazonaws.com",
-    "cloudtrail.amazonaws.com",
-    "config.amazonaws.com",
-    "detective.amazonaws.com",
-    "fms.amazonaws.com",
-    "guardduty.amazonaws.com",
-    "inspector2.amazonaws.com",
-    "ipam.amazonaws.com",
-    "macie.amazonaws.com",
-    "malware-protection.guardduty.amazonaws.com",
-    "member.org.stacksets.cloudformation.amazonaws.com",
-    "ram.amazonaws.com",
-    "securityhub.amazonaws.com",
-    "securitylake.amazonaws.com",
-    "servicecatalog.amazonaws.com",
-    "ssm.amazonaws.com",
-    "sso.amazonaws.com",
-    "storage-lens.s3.amazonaws.com",
-    "tagpolicies.tag.amazonaws.com"
-  ]
-  enabled_policy_types = [
-    "AISERVICES_OPT_OUT_POLICY",
-    "BACKUP_POLICY",
-    "SERVICE_CONTROL_POLICY",
-    "TAG_POLICY"
-  ]
-  feature_set = "ALL"
-}
-
 resource "aws_s3_bucket" "bucket" {
   bucket = "${var.name}-${data.aws_caller_identity.current.id}-${data.aws_region.current.id}"
 }
