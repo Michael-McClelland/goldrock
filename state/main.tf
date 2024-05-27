6c0c809de551bd3961e0dfffd206f675527cfe10
Michael McClelland <mikemcclelland@gmail.com>
2024-05-26 22:09:31 -0400

    code

--- state/main.tf
+++ state/main.tf
@@ -718,12 +718,9 @@ data "aws_iam_policy_document" "dynamodb" {
       test     = "ForAllValues:StringLike"
       variable = "dynamodb:LeadingKeys"
       values = [
-        "goldrock-tfstate-529162655134-us-east-2/*"
+        "goldrock-tfstate-529162655134-us-east-2/*-*-*-$${aws:PrincipalAccount}-*"
       ]
     }
-
-
-
 
     #goldrock-tfstate-529162655134-us-east-2/us-east-2-224172266018-ec2_serial_console-md5
     # condition {
