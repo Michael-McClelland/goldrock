040d45a046fb9313d421ddf0482b87c3b60a5066
Michael McClelland <mikemcclelland@gmail.com>
2024-06-08 18:06:11 -0400

    code

---
+++ regional/us-east-2/security_services/cloudtrail.tf
@@ -0,0 +1,8 @@
+resource "aws_cloudtrail" "cloudtrail" {
+  name                          = "goldrockCloudTrail"
+  s3_bucket_name                = aws_s3_bucket.cloudtrail.name
+  is_organization_trail         = true
+  is_multi_region_trail         = true
+  include_global_service_events = true
+  kms_key_id                    = aws_kms_key.key.arn
+}
