4318d00bccccd97d27455393a77ff8fa3174d76f
Michael McClelland <mikemcclelland@gmail.com>
2024-06-09 03:43:28 -0400

    code

--- regional/us-east-2/security_services/cloudtrail.tf
+++ regional/us-east-2/security_services/cloudtrail.tf
@@ -1,19 +1,19 @@
-# resource "aws_cloudtrail" "cloudtrail" {
-#   name                          = "goldrock"
-#   s3_bucket_name                = aws_s3_bucket.cloudtrail.id
-#   is_organization_trail         = true
-#   is_multi_region_trail         = true
-#   include_global_service_events = true
-#   enable_log_file_validation    = true
-#   s3_key_prefix                 = "goldrock"
-#   kms_key_id                    = aws_kms_key.key.arn
+resource "aws_cloudtrail" "cloudtrail" {
+  name                          = "goldrock"
+  s3_bucket_name                = aws_s3_bucket.cloudtrail.id
+  is_organization_trail         = true
+  is_multi_region_trail         = true
+  include_global_service_events = true
+  enable_log_file_validation    = true
+  s3_key_prefix                 = "goldrock"
+  kms_key_id                    = aws_kms_key.key.arn
 
-#   event_selector {
+  event_selector {
 
-#     include_management_events = true
-#     read_write_type           = "All"
-#   }
-# }
+    include_management_events = true
+    read_write_type           = "All"
+  }
+}
 
 
 
