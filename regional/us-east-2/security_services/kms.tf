8180596f4acd64a9b5ce582844134b85ff7b920f
Michael McClelland <mccmcc@amazon.com>
2024-06-13 13:12:30 -0400

    code

--- regional/us-east-2/security_services/kms.tf
+++ regional/us-east-2/security_services/kms.tf
@@ -1,29 +1,5 @@
 data "aws_iam_policy_document" "keypolicy" {
 
-  # statement {
-  #   sid = "CloudTrailDescribe"
-  #   principals {
-  #     type = "Service"
-  #     identifiers = [
-  #       "cloudtrail.amazonaws.com"
-  #     ]
-  #   }
-  #   effect = "Allow"
-  #   actions = [
-  #     "kms:DescribeKey"
-  #   ]
-  #   resources = [
-  #     "*",
-  #   ]
-  #   condition {
-  #     test     = "StringEquals"
-  #     variable = "aws:SourceOrgID"
-  #     values = [
-  #       "${data.aws_organizations_organization.current.id}"
-  #     ]
-  #   }
-  # }
-
   statement {
     sid = "KMSCloudTrail"
     principals {
