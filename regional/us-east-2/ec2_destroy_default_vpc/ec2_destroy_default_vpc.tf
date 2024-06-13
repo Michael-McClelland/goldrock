6bddf284fab367eed3feac8e9885acd64bd460c5
Michael McClelland <mccmcc@amazon.com>
2024-06-13 15:06:22 -0400

    code

--- regional/us-east-2/ec2_destroy_default_vpc/ec2_destroy_default_vpc.tf
+++ regional/us-east-2/ec2_destroy_default_vpc/ec2_destroy_default_vpc.tf
@@ -1,6 +1,13 @@
-# resource "aws_default_vpc" "default" {
-#   force_destroy = true
-# }
+resource "awsutils_default_vpc_deletion" "default" {}
+
+terraform {
+  required_providers {
+    awsutils = {
+      source = "cloudposse/awsutils"
+    }
+  }
+}
+
 terraform {
   backend "s3" {
   }
