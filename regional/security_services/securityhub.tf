0b9d73f6092ca23e2db7b0de598b5af3b94e0983
Michael McClelland <mccmcc@amazon.com>
2024-06-23 18:51:49 -0400

    define a securityhub

--- regional/security_services/securityhub.tf
+++ regional/security_services/securityhub.tf
@@ -1,3 +1,9 @@
+resource "aws_securityhub_account" "securityhub" {
+  auto_enable_controls      = false
+  control_finding_generator = "SECURITY_CONTROL"
+  enable_default_standards  = false
+}
+
 resource "aws_securityhub_finding_aggregator" "aws_securityhub_finding_aggregator" {
   linking_mode = "ALL_REGIONS"
 }
