0bab49ffce08992c4488af93b7f5aff1cfd1d5ac
Michael McClelland <mccmcc@amazon.com>
2024-06-23 22:08:42 -0400

    code

--- regional/security_services/securityhub.tf
+++ regional/security_services/securityhub.tf
@@ -14,25 +14,25 @@ resource "aws_securityhub_organization_configuration" "aws_securityhub_organizat
   ]
 }
 
-# resource "aws_securityhub_configuration_policy" "example" {
-#   name        = "Example"
-#   description = "This is an example configuration policy"
+resource "aws_securityhub_configuration_policy" "aws_securityhub_configuration_policy" {
+  name        = "default"
+  description = "default"
 
-#   configuration_policy {
-#     service_enabled = true
-#     enabled_standard_arns = [
-#       "arn:aws:securityhub:us-east-1::standards/aws-foundational-security-best-practices/v/1.0.0",
-#       "arn:aws:securityhub:::ruleset/cis-aws-foundations-benchmark/v/1.2.0",
-#     ]
-#     security_controls_configuration {
-#       disabled_control_identifiers = []
-#     }
-#   }
+  configuration_policy {
+    service_enabled = true
+    enabled_standard_arns = [
+      "arn:aws:securityhub:us-east-1::standards/aws-foundational-security-best-practices/v/1.0.0",
+      "arn:aws:securityhub:::ruleset/cis-aws-foundations-benchmark/v/1.2.0",
+    ]
+    security_controls_configuration {
+      disabled_control_identifiers = []
+    }
+  }
 
-#   depends_on = [aws_securityhub_organization_configuration.example]
-# }
+  depends_on = [aws_securityhub_organization_configuration.example]
+}
 
-# resource "aws_securityhub_configuration_policy_association" "root_example" {
-#   target_id = "r-abcd"
-#   policy_id = aws_securityhub_configuration_policy.example.id
-# }
+resource "aws_securityhub_configuration_policy_association" "root" {
+  target_id = data.aws_organizations_organization.current.roots[0].id
+  policy_id = aws_securityhub_configuration_policy.aws_securityhub_configuration_policy.id
+}
