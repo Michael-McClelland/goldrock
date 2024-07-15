f6368826dbcb430ebeaf90b6ee84855aa840ba37
Michael McClelland <mccmcc@amazon.com>
2024-07-14 22:21:48 -0400

    sort

--- global/iam_password_policy/iam_password_policy.tf
+++ global/iam_password_policy/iam_password_policy.tf
@@ -1,13 +1,13 @@
 resource "aws_iam_account_password_policy" "password_policy" {
   allow_users_to_change_password = true
+  hard_expiry = true
+  max_password_age = 90
   minimum_password_length        = 16
+  password_reuse_prevention = 24
   require_lowercase_characters   = true
   require_numbers                = true
   require_symbols                = true
   require_uppercase_characters   = true
-  password_reuse_prevention = 24
-  max_password_age = 90
-  hard_expiry = true
 }
 terraform {
   backend "s3" {
