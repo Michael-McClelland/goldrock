fbed1f96ade03deefb363b9828445ff68bd64b4b
Michael McClelland <mccmcc@amazon.com>
2024-09-11 21:35:35 -0400

    code

---
+++ python/additional_regions_state.py
@@ -0,0 +1,100 @@
+import os
+import boto3
+from boto3 import session
+import botocore
+
+# ######
+# os.environ['PY_REGION'] = 'us-west-2'
+# os.environ['PY_HOME_REGION'] = 'us-east-2'
+# ######
+
+
+
+account_id = boto3.client('sts').get_caller_identity().get('Account')
+session = session.Session()
+partition = session.get_partition_for_region(region_name=os.environ['PY_HOME_REGION'])
+
+
+s3_client = boto3.client('s3')
+try:
+  s3_response = s3_client.head_bucket(
+      Bucket='goldrock-tfstate-' + account_id + '-' + os.environ['PY_REGION'],
+      ExpectedBucketOwner=account_id
+  )
+  bucket_exists = True
+except botocore.exceptions.ClientError:
+  bucket_exists = False
+if not bucket_exists:
+  s3_client.create_bucket(
+  Bucket='goldrock-tfstate-' + account_id + '-' + os.environ['PY_REGION'], 
+  CreateBucketConfiguration={'LocationConstraint': os.environ['PY_REGION']}
+  )
+######
+
+kms_home_region_client = boto3.client('kms', region_name=os.environ['PY_HOME_REGION'])
+kms_home_region_response = kms_home_region_client.describe_key(
+    KeyId='arn:' +  partition + ':kms:' + os.environ['PY_HOME_REGION'] + ':' + account_id + ':alias/goldrock-tfstate'
+)
+kms_client = boto3.client('kms',region_name=os.environ['PY_REGION'])
+try:
+  kms_replica_region_response = kms_client.describe_key(
+      KeyId='arn:' +  partition + ':kms:' + os.environ['PY_REGION'] + ':' + account_id + ':key/' + kms_home_region_response['KeyMetadata']['KeyId']
+  )
+  key_exists = True
+except kms_client.exceptions.NotFoundException:
+  key_exists = False
+if not key_exists:
+  response = kms_client.replicate_key(
+    KeyId=kms_home_region_response['KeyMetadata']['KeyId'],
+    ReplicaRegion=os.environ['PY_REGION'],
+    BypassPolicyLockoutSafetyCheck=False
+  )
+try:
+  kms_replica_region_response_alias = kms_client.describe_key(
+      KeyId='arn:' +  partition + ':kms:' + os.environ['PY_REGION'] + ':' + account_id + ':alias/goldrock-tfstate'
+  )
+  alias_exists = True
+except kms_client.exceptions.NotFoundException:
+  alias_exists = False
+if not alias_exists:
+  response = kms_client.create_alias(
+      AliasName='alias/goldrock-tfstate',
+      TargetKeyId=kms_home_region_response['KeyMetadata']['KeyId']
+  )
+######
+dynamodb_client = boto3.client('dynamodb',region_name=os.environ['PY_REGION'])
+try:
+  dynamodb_describe_response = dynamodb_client.describe_table(
+      TableName='goldrock-tfstate-' + account_id +'-' + os.environ['PY_REGION']
+  )
+  dynamodb_table_exists = True
+except dynamodb_client.exceptions.NotFoundException:
+  dynamodb_table_exists = False
+if not dynamodb_table_exists:
+  dynamodb_response = dynamodb_client.create_table(
+      AttributeDefinitions=[
+          {
+              'AttributeName': 'LockID',
+              'AttributeType': 'S'
+          },
+      ],
+      TableName='goldrock-tfstate-' + account_id +'-' + os.environ['PY_REGION'],
+      KeySchema=[
+          {
+              'AttributeName': 'LockID',
+              'KeyType': 'HASH'
+          },
+      ],
+      BillingMode='PROVISIONED',
+      ProvisionedThroughput={
+          'ReadCapacityUnits': 5,
+          'WriteCapacityUnits': 5
+      },
+      SSESpecification={
+          'Enabled': True,
+          'SSEType': 'KMS',
+          'KMSMasterKeyId': 'arn:' +  partition + ':kms:' + os.environ['PY_REGION'] + ':' + account_id + ':alias/goldrock-tfstate'
+      },
+      TableClass='STANDARD',
+      DeletionProtectionEnabled=True
+)
