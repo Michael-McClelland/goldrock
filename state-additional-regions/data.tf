6981781787dd479c8f4a690bbf6ffd9a57cab29d
Michael McClelland <mccmcc@amazon.com>
2024-09-11 22:26:11 -0400

    code

--- state-additional-regions/data.tf
+++ state-additional-regions/data.tf
@@ -4,5 +4,5 @@ data "aws_organizations_organization" "current" {}
 data "aws_partition" "current" {}
 data "aws_region" "current" {}
 data "aws_kms_key" "primary_cmk" {
-  key_id = "arn:${data.aws_partition.current.partition}kms:${var.HOME_REGION}:${data.aws_caller_identity.current.id}:alias/goldrock-tfstate"
+  key_id = "arn:${data.aws_partition.current.partition}:kms:${var.HOME_REGION}:${data.aws_caller_identity.current.id}:alias/goldrock-tfstate"
 }
