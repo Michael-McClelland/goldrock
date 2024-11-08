ab58ce902f394d130937b8de53a7f82b06a28150
Michael McClelland <mccmcc@amazon.com>
2024-11-08 15:49:53 -0500

    tier

--- organization/ssm.tf
+++ organization/ssm.tf
@@ -2,6 +2,7 @@ resource "aws_ssm_parameter" "goldrock_security_account_id" {
   name        = "/goldrock/security_account_id"
   description = "goldrock security account id"
   type        = "SecureString"
+  tier        = "Advanced"
   value       = module.organization_structure.security_account
   key_id      = data.aws_kms_key.goldrock_tfstate.arn
 }
