7f1ffe7ef11ef22a2b6cc2d30a07f5d5239c9f9c
Michael McClelland <mccmcc@amazon.com>
2024-11-11 13:44:06 -0500

    code

--- regional/additional_regions/data.tf
+++ regional/additional_regions/data.tf
@@ -6,5 +6,7 @@ data "aws_ssm_parameter" "security_account_id" {
   name = "arn:${data.aws_partition.current.partition}:ssm:${data.aws_region.current.id}:${data.aws_organizations_organization.organization.master_account_id}:parameter/goldrock/security_account_id"
 }
 data "aws_iam_role" "configservice" {
-  name = "aws-service-role/config.amazonaws.com/AWSServiceRoleForConfig"
+  name = "AWSServiceRoleForConfig"
 }
+
+#aws-service-role/config.amazonaws.com/
