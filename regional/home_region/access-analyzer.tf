7ce2052d3b80dd80c45e35c44e00ca1c618b35cb
Michael McClelland <mccmcc@amazon.com>
2024-11-12 10:59:29 -0500

    unused access

--- regional/home_region/access-analyzer.tf
+++ regional/home_region/access-analyzer.tf
@@ -1,3 +1,8 @@
 resource "aws_accessanalyzer_analyzer" "aws_accessanalyzer_analyzer" {
   analyzer_name = "account"
 }
+
+resource "aws_accessanalyzer_analyzer" "aws_accessanalyzer_analyzer_unused_access" {
+  analyzer_name = "unused-access"
+  type          = "ACCOUNT_UNUSED_ACCESS"
+}
