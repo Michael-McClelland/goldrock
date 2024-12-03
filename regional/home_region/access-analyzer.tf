131471a77dc2a82bdc0da410c2356ef5c9c56f66
Michael McClelland <30412622+Michael-McClelland@users.noreply.github.com>
2024-12-03 14:19:13 -0500

    unused-access-expense

--- regional/home_region/access-analyzer.tf
+++ regional/home_region/access-analyzer.tf
@@ -2,7 +2,7 @@ resource "aws_accessanalyzer_analyzer" "aws_accessanalyzer_analyzer" {
   analyzer_name = "account"
 }
 
-resource "aws_accessanalyzer_analyzer" "aws_accessanalyzer_analyzer_unused_access" {
-  analyzer_name = "unused-access"
-  type          = "ACCOUNT_UNUSED_ACCESS"
-}
+# resource "aws_accessanalyzer_analyzer" "aws_accessanalyzer_analyzer_unused_access" {
+#   analyzer_name = "unused-access"
+#   type          = "ACCOUNT_UNUSED_ACCESS"
+# }
