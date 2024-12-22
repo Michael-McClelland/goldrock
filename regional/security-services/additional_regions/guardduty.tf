9414a99b0e2d87b716e2382fe9e99074103e4302
Michael McClelland <30412622+Michael-McClelland@users.noreply.github.com>
2024-12-22 16:45:35 -0500

    code

--- regional/security-services/additional_regions/guardduty.tf
+++ regional/security-services/additional_regions/guardduty.tf
@@ -17,13 +17,13 @@ resource "aws_guardduty_organization_configuration_feature" "aws_guardduty_organ
 
   detector_id = data.aws_guardduty_detector.aws_guardduty_detector.id
   name        = each.value
-  status      = "ENABLED"
+  auto_enable = "ALL"
 }
 
 resource "aws_guardduty_organization_configuration_feature" "aws_guardduty_organization_configuration_feature_runtime_monitoring" {
   detector_id = data.aws_guardduty_detector.aws_guardduty_detector.id
   name        = "RUNTIME_MONITORING"
-  status      = "ENABLED"
+  auto_enable = "ALL"
 
   additional_configuration {
     name   = "EKS_ADDON_MANAGEMENT"
