310cff60f660df5da37a9487fc3adb32436f65ea
Michael McClelland <mccmcc@amazon.com>
2025-01-06 10:34:28 -0500

    abstract organization configuration

--- regional/security-services/additional_regions/s3.tf
+++ regional/security-services/additional_regions/s3.tf
@@ -199,27 +199,27 @@ data "aws_iam_policy_document" "config" {
   #   }
   # }
 
-  # statement {
-  #   effect = "Deny"
-  #   principals {
-  #     type        = "AWS"
-  #     identifiers = ["*"]
-  #   }
-  #   actions = [
-  #     "s3:*",
-  #   ]
+  statement {
+    effect = "Deny"
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    actions = [
+      "s3:*",
+    ]
 
-  #   resources = [
-  #     "${aws_s3_bucket.config.arn}/*",
-  #     "${aws_s3_bucket.config.arn}",
-  #   ]
+    resources = [
+      "${aws_s3_bucket.config.arn}/*",
+      "${aws_s3_bucket.config.arn}",
+    ]
 
-  #   condition {
-  #     test     = "Bool"
-  #     variable = "aws:SecureTransport"
-  #     values   = ["false"]
-  #   }
-  # }
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+  }
 
 }
 
