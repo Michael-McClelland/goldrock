0e6ac4264786a15863f3c01d62d307408b0b1b5e
Michael McClelland <mccmcc@amazon.com>
2025-01-26 16:12:38 -0500

    resource control policy

---
+++ organization/resource-control-policies.tf
@@ -0,0 +1,128 @@
+data "aws_iam_policy_document" "resource_control_standard_policy" {
+
+
+  statement {
+    principals {
+      type = "AWS"
+      identifiers = [
+        "*"
+      ]
+    }
+    effect = "Deny"
+    actions = [
+      "s3:*",
+    ]
+    resources = [
+      "*",
+    ]
+    condition {
+      test     = "NumericLessThan"
+      variable = "s3:TlsVersion"
+      values = [
+        "1.3"
+      ]
+    }
+  }
+
+  statement {
+    principals {
+      type = "AWS"
+      identifiers = [
+        "*"
+      ]
+    }
+    effect = "Deny"
+    actions = [
+
+      "s3:*",
+    ]
+    resources = [
+      "*",
+    ]
+    condition {
+      test     = "Null"
+      variable = "s3:x-amz-server-side-encryption-aws-kms-key-id"
+      values = [
+        "true"
+      ]
+    }
+  }
+
+  statement {
+    principals {
+      type = "AWS"
+      identifiers = [
+        "*"
+      ]
+    }
+    effect = "Deny"
+    actions = [
+      "kms:*",
+      "s3:*",
+      "secretsmanager:*",
+      "sqs:*",
+      "sts:*",
+    ]
+    resources = [
+      "*",
+    ]
+    condition {
+      test     = "BoolIfExists"
+      variable = "aws:SecureTransport"
+      values = [
+        "false"
+      ]
+    }
+  }
+
+  statement {
+    principals {
+      type = "AWS"
+      identifiers = [
+        "*"
+      ]
+    }
+    effect = "Deny"
+    actions = [
+      "s3:*",
+      "secretsmanager:*",
+      "sqs:*",
+    ]
+    resources = [
+      "*",
+    ]
+    condition {
+      test     = "StringNotEqualsIfExists"
+      variable = "aws:SourceOrgID"
+      values = [
+        data.aws_organizations_organization.organization.id
+      ]
+    }
+    condition {
+      test     = "Bool"
+      variable = "aws:PrincipalIsAWSService"
+      values = [
+        "true"
+      ]
+    }
+    condition {
+      test     = "Null"
+      variable = "aws:SourceAccount"
+      values = [
+        "false"
+      ]
+    }
+  }
+}
+
+resource "aws_organizations_policy" "resource_control_standard_policy" {
+  name    = "resource-control-standard-policy"
+  type = "RESOURCE_CONTROL_POLICY"
+  content = data.aws_iam_policy_document.resource_control_standard_policy.json
+}
+
+resource "aws_organizations_policy_attachment" "resource_control_standard_policy" {
+  policy_id = aws_organizations_policy.resource_control_standard_policy.id
+  target_id = data.aws_organizations_organization.organization.roots[0].id
+}
+
