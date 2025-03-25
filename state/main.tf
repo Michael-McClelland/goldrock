5573298eb8fed348b881d08c0091f3bd1c76e2c5
Michael McClelland <mikemcclelland@gmail.com>
2025-03-25 19:41:36 -0400

    lock-removal

--- state/main.tf
+++ state/main.tf
@@ -22,7 +22,8 @@ data "aws_iam_policy_document" "bucket" {
     actions = [
       "s3:GetObject",
       "s3:ListBucket",
-      "s3:PutObject"
+      "s3:PutObject",
+      "s3:DeleteObject"
     ]
 
     resources = [
