a4e591ac6e45d3fedc7213d076eba8d5e63ffd39
Michael McClelland <30412622+Michael-McClelland@users.noreply.github.com>
2025-08-14 17:56:39 -0400

    code

--- regional/home_region/ec2-ami-block-public-sharing.tf
+++ regional/home_region/ec2-ami-block-public-sharing.tf
@@ -1,3 +1,7 @@
 resource "aws_ec2_image_block_public_access" "image_block_public_access" {
   state = "block-new-sharing"
 }
+
+resource "aws_ebs_snapshot_block_public_access" "aws_ebs_snapshot_block_public_access" {
+  state = "block-all-sharing"
+}
